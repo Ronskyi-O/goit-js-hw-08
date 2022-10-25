9baf6ba87c996af5ff5c6363b2ba293459a5c899
Oleksandr Ronskyi <oleksandr.ronskyi@gmail.com>
2022-10-24 20:02:44 -0400

    task 2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import Player from '@vimeo/player';
+import Throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+
+const onPlay = function (data) {
+    const currentTime = JSON.stringify(data.seconds)
+    localStorage.setItem("videoplayer-current-time", currentTime)
+};
+
+player.on('timeupdate', Throttle(onPlay, 1000));
+
+
+const resumePlayback = function () {
+    const savedTime = localStorage.getItem("videoplayer-current-time");
+        if (savedTime) {
+            player.setCurrentTime(savedTime);
+        }
+    }
+
+player.on("loaded", resumePlayback);
 
